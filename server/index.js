8719fbc5e9ff0505a7d8b6088467856da48ba9fe
Jisol Park <jisolp7@gmail.com>
2025-04-23 08:08:09 -0700

    for deploy

--- server/index.js
+++ server/index.js
@@ -46,3 +46,11 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
